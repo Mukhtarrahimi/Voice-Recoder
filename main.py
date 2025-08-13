882fc9d15be3d0b48866df2273b6510510a5f810
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-13 20:52:23 +0430

    Add bg color & icon.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ root = Tk()
 root.title("Voice Recorder")
 root.geometry("600x700+400+80")
 root.resizable(False, False)
+root.configure(background="#4a4a4a")
 
+# icon
+image_icon = PhotoImage(file="Record.png")
+root.iconphoto(False, image_icon)
 
 root.mainloop()
