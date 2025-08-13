70a70d072a9d34d14f2524f577c7f841aa83be74
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-13 20:48:29 +0430

    Add initilaize tkinter.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
+
+root = Tk()
+root.title("Voice Recorder")
+root.geometry("600x700+400+80")
+root.resizable(False, False)
+
+
+root.mainloop()
