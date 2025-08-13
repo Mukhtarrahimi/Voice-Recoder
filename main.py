7efce73fcb492239eac39a6808ea0cf27a30f2c0
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-13 21:11:43 +0430

    Add buttons.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
 
+
+
+
+
 root = Tk()
 root.title("Voice Recorder")
 root.geometry("600x700+400+80")
@@ -22,4 +26,11 @@ Label(text="Voice Recorder", font=("Arial", 30, "bold"), bg="#4a4a4a", fg="white
 duration = StringVar()
 Entry(root, textvariable=duration, font=("Arial", 20), width=15).pack(pady=10)
 Label(text="Enter Duration in seconds", font=("Arial", 15), bg="#4a4a4a", fg="white").pack()
+
+# button
+record = Button(root, text="Record", font=("Arial", 20), bg="#111111", fg="white", border=0, command=Record).pack(pady=20)
+
+
+
+
 root.mainloop()
